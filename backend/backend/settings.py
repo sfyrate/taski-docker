2a923ba42659458b335d1fd8bd71607dc90b4433
Игорь Семенов <sfyrate@yandex.ru>
2024-07-20 15:55:43 +0300

    test1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('D_KEY')
+SECRET_KEY = os.getenv('D_KEY', 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
 
 DEBUG = False
 ALLOWED_HOSTS = ['158.160.5.22', 'tasky.ddns.net', '127.0.0.1', 'localhost']
